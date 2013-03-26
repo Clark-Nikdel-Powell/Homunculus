d9d4884de0aa4f7c4c4d35514077389bab105a20
Chris Roche <rodaine+github@gmail.com>
2013-03-26 14:23:56 -0400

    Stripped out php from brackets (internal decision) and implemented cnp_theme_url

--- header.php
+++ header.php
@@ -4,42 +4,42 @@
 <!--[if IE 9]>         <html class="ie ie9">               <![endif]-->
 <!--[if gt IE 9]><!--> <html>                              <!--<![endif]-->
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta charset="<? bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<title><?php wp_title('&lsaquo;', true, 'right'); ?></title>
+	<title><? wp_title('&lsaquo;', true, 'right'); ?></title>
 
-	<meta name="viewport" content="width=device-width">
-	<meta name="author" content="Clark/Nikdel/Powell">
-	<meta name="description" content="<?php do_action('cnp_description'); ?>">
+	<meta name="viewport"    content="width=device-width">
+	<meta name="author"      content="Clark/Nikdel/Powell">
+	<meta name="description" content="<? do_action('cnp_description'); ?>">
 
-	<link href="<?=WP_THEME_URL?>/style.css" rel="stylesheet" type="text/css" media="screen" />
-	<link href="<?=WP_THEME_URL?>/print.css" rel="stylesheet" type="text/css" media="print" />
-	<link href="<?=WP_THEME_URL?>/favicon.png" rel="icon" />
-	<link rel="alternate" type="application/rss+xml" title="<? bloginfo('name'); ?>" href="<? bloginfo('rss2_url'); ?>" />
+	<link rel="stylesheet" href="<?=cnp_theme_url('style.css');?>" media="screen">
+	<!--[if IE]><link rel="stylesheet" href="<?=cnp_theme_url('ie.css');?>" media="screen"><![endif]-->
+	<link rel="stylesheet" href="<?=cnp_theme_url('print.css');?>" media="print">
+
+	<link rel="icon" href="<?=cnp_theme_url('favicon.png');?>">
+	<link rel="alternate" type="application/rss+xml" title="<? bloginfo('name'); ?>" href="<? bloginfo('rss2_url'); ?>">
 
 	<!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 	
-<? wp_head(); ?>
-
+	<? wp_head(); ?>
 </head>
+<body itemscope itemtype="http://schema.org/WebPage" <? body_class(); ?>>
 
-<body <? body_class(); ?>>
-
-<header class="site">
+<header class="site" itemscope itemtype="http://schema.org/WPHeader" role="banner">
 	
-	<h1 id="logo"><a href="/"><?php bloginfo('name'); ?></a></h1>
+	<h1 class="logo"><a href="<?=home_url()?>"><? bloginfo('name'); ?></a></h1>
 	
 	<?
 	wp_nav_menu(array(
-		'menu'         => 'Main Menu'
-	,	'container'    => 'nav'
-	,	'container_id' => 'site'
-	,	'depth'        => 1
-	,	'fallback_cb'  => false
+		'menu'            => 'Main Menu'
+	,	'container'       => 'nav'
+	,	'container_class' => 'site'
+	,	'depth'           => 1
+	,	'fallback_cb'     => false
 	));
 	?>
 	
 </header>
 
-<div class="main">
+<div class="main" role="main">
