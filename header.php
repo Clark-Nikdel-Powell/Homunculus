63d5089290cfd41465eb0ef35b838dd72a11ca12
Chris Roche <rodaine+github@gmail.com>
2013-03-26 11:58:38 -0400

    Header Cleanup
    - Better formatted DOCTYPE & html elements
    - <? -> <?php where needed
    - title reduced to wp_title() [filter added to CNP-Core]
    - meta fields cleaned up
    - HTML5 shim made protocol insensitive
    - logo name = bloginfo name

--- header.php
+++ header.php
@@ -1,29 +1,24 @@
-<!doctype html>
-<!--[if IE 7]>    <html class="ie ie7 lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="ie ie8 lt-ie9" lang="en"> <![endif]-->
-<!--[if IE 9]>    <html class="ie ie9" lang="en"> <![endif]-->
-<!--[if gt IE 9]><!--> <html lang="en"> <!--<![endif]-->
-
+<!DOCTYPE html>
+<!--[if IE 7]>         <html class="ie ie7 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="ie ie8 lt-ie9">        <![endif]-->
+<!--[if IE 9]>         <html class="ie ie9">               <![endif]-->
+<!--[if gt IE 9]><!--> <html>                              <!--<![endif]-->
 <head>
-
-	<meta charset="<? bloginfo('charset'); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<!--<meta name="viewport" content="width=device-width">-->
-	<meta name="author" content="Clark/Nikdel/Powell" />
-	<meta name="description" content="">
 
-	<title><?
-		wp_title('&lsaquo;', true, 'right');
-		bloginfo('name');
-		if ( is_home() && get_bloginfo('description') ) { print " &rsaquo; "; bloginfo('description'); }
-	?></title>
+	<title><?php wp_title('&lsaquo;', true, 'right'); ?></title>
+
+	<meta name="viewport" content="width=device-width">
+	<meta name="author" content="Clark/Nikdel/Powell">
+	<meta name="description" content="<?php do_action('cnp_description'); ?>">
 
 	<link href="<?=WP_THEME_URL?>/style.css" rel="stylesheet" type="text/css" media="screen" />
 	<link href="<?=WP_THEME_URL?>/print.css" rel="stylesheet" type="text/css" media="print" />
 	<link href="<?=WP_THEME_URL?>/favicon.png" rel="icon" />
 	<link rel="alternate" type="application/rss+xml" title="<? bloginfo('name'); ?>" href="<? bloginfo('rss2_url'); ?>" />
 
-	<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 	
 <? wp_head(); ?>
 
@@ -33,7 +28,7 @@
 
 <header class="site">
 	
-	<h1 id="logo"><a href="/">CLIENT NAME</a></h1>
+	<h1 id="logo"><a href="/"><?php bloginfo('name'); ?></a></h1>
 	
 	<?
 	wp_nav_menu(array(
