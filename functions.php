59577eb8f0c5eab7a4d74f1bd17bd23038151932
Chris Roche <rodaine@gmail.com>
2013-06-21 16:00:24 -0400

    THIS IS SPARTA~!!!

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?
+/**
+ * NO DIRECT PHP CODE SHOULD BE IN THIS FILE. 
+ * THIS FILE SHOULD ONLY BE A LIST OF require_once() 
+ * CALLS TO THE *MODULAR* FUNCTION FILES IN /functions
+ */
 
 require_once cnp_theme_path('functions/enqueues.php');
-
-require_once cnp_theme_path('functions-general.php');
