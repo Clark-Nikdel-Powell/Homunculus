e0be52581c5a6c842a038f0fbd4ff130e8c29b76
Taylor Gorman <me@taylorpatrickgorman.com>
2013-11-01 16:22:22 -0400

    I deleted codekit-config.json for some reason, idk

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ images_dir = "images"
 javascripts_dir = "scripts"
 fonts_dir = "fonts"
 
-output_style = :nested
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
@@ -22,4 +22,3 @@ color_output = false
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-preferred_syntax = :scss
