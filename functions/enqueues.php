91465c4d4177e4f1db83810dd88e8abf140e44d7
Samuel Mello <sam@clarknikdelpowell.com>
2014-01-07 13:08:19 -0500

    Modified script to point the correct sub dir / file

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -3,6 +3,6 @@
 add_action(
 	'wp_enqueue_scripts',
 	function() {
-		wp_enqueue_script('cnp-app', cnp_theme_url('scripts/app.min.js'), array('jquery'), false, true);
+		wp_enqueue_script('cnp-app', cnp_theme_url('js/site.js'), array('jquery'), false, true);
 	}
 );
