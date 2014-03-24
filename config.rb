820bc913c96db4b2a2c26ac8590ea7ed528c493f
Taylor Gorman <me@taylorpatrickgorman.com>
2014-03-24 11:26:30 -0400

    Shortened images and javascript folder names

--- config.rb
+++ config.rb
@@ -4,8 +4,8 @@
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "scripts"
+images_dir = “img”
+javascripts_dir = “js”
 fonts_dir = "fonts"
 
 output_style = :compressed
