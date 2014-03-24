d412f8be03058f817eb00cc8ac4d5369ee68dc48
Taylor Gorman <me@taylorpatrickgorman.com>
2014-03-24 11:31:09 -0400

    Code formatting

--- index.php
+++ index.php
@@ -2,24 +2,25 @@
 get_header('int');
 if ( is_search() ) {$type = 'search';}
 else {$type = $post->post_type;}
-if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+if (have_posts()) { while (have_posts()) { the_post(); ?>
 
 	<div <? post_class(); ?>>
 
 		<? get_template_part('content', $type); ?>
 
-	</div><?/* post_class */?>
+	</div>
 
-<? endwhile; pagination('&larr; Back', 'More &rarr;'); else : ?>
+<? } pagination('&larr; Back', 'More &rarr;'); } else { ?>
 
 	<div class="hentry">
 
 		<p class="summary">There is nothing in this section right now. Check back later for updates!</p>
 
-	</div><?/* hentry */?>
+	</div>
 
 <?
-endif;
+}
 
 get_footer('int');
 ?>
